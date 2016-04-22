84ccbfa8f57b532f07cd94ccc24a9190baa5a7e1
esysop <leehassel@gmail.com>
2016-04-22 11:02:36 -0500

    added another writeline

---
+++ CollaborationTestSweepStakes/SystemArrayString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollaborationTestSweepStakesLee
+	{
+	public class SystemArrayString : ICustomString
+		{
+
+		char[] manipulatedCharArray;
+
+		public SystemArrayString (string manipulatedString)
+			{
+			manipulatedCharArray = manipulatedString.ToCharArray();
+			}
+		public override string ToString ()
+			{
+			string finalString = null;
+			foreach (char character in manipulatedCharArray)
+				{
+				finalString += character;
+				
+				}
+			return finalString;
+			}
+
+
+
+		public void Insert (int startIndex, string stringToInsert)
+			{
+			
+			char[] insertedCharsArray = stringToInsert.ToCharArray();
+			
+			}
+
+		public int Length ()
+			{
+			return manipulatedCharArray.Length;
+			}
+
+		public void Remove (int startIndex, int numCharsToRemove)
+			{
+			throw new NotImplementedException();
+			}
+		}
+	}
---
+++ CollaborationTestSweepStakes/SystemString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollaborationTestSweepStakesLee
+	{
+	 public class SystemString : ICustomString
+		{
+		public string StringToManipulate ;
+
+		public SystemString (string inputString)
+			{
+			StringToManipulate = inputString;
+			}
+		public override string ToString ()
+			{
+			return StringToManipulate.ToString();
+			}
+			
+		public void Insert(int startIndex, string stringToInsert )
+
+			{
+			StringToManipulate.Insert( startIndex, stringToInsert);
+			}
+		public void Remove (int startIndex, int numCharsToRemove)
+			{
+			StringToManipulate.Remove(startIndex, numCharsToRemove);
+			}
+		public int Length ()
+			{
+			return StringToManipulate.Length;
+			}
+		
+	
+		}
+	}
