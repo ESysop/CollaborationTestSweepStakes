3307fb66592c1399a87976ceea614f8ee6524ee5
esysop <leehassel@gmail.com>
2016-04-22 10:59:41 -0500

    second commit

---
+++ CollaborationTestSweepStakes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollaborationTestSweepStakesLee
+	{
+	class Program
+		{
+		
+		static void Main (string[] args)
+			{
+			string a = "hello world";
+			string b = "greetings";
+			SystemString test = new SystemString( a);
+			CustomLinkiedListString customLinkedListString = new CustomLinkiedListString();
+			SystemArrayString systemArrayString = new SystemArrayString(b);
+			SystemLinkedListString sytemLinkedListString = new SystemLinkedListString();
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+			Console.WriteLine("The BitSlapper was all over this shit!");
+		
+			
+			}
+		}
+	}
